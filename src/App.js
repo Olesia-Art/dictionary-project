9ff3756ab12ee70301aeb0d5d02031903bcd7a26
Olesia-Art <olesiasmail1@gmail.com>
2022-10-01 18:54:20 +0300

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Olesia Artibiakina</footer>
+      </div>
+    </div>
+  );
 }
