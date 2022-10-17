7700b208691b0213eb948e32afb6559f860ad9c1
Olesia-Art <olesiasmail1@gmail.com>
2022-10-17 14:23:48 +0300

    Added links to social networks

--- src/App.js
+++ src/App.js
@@ -12,7 +12,32 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="wine" />
         </main>
-        <footer className="App-footer">Coded by Olesia Artibiakina</footer>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/olesia-artibiakina-00030ab9/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Olesia Artibiakina{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Olesia-Art/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://resplendent-creponne-dee3d2.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
